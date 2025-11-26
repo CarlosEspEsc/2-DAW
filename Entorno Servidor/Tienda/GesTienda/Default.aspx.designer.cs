ae600bb3637d1f4e7a6505c44009bf886e8b1c15
CarlosEspEsc <cespinosaescrig@gmail.com>
2025-11-26 14:30:31 +0100

    avances

--- Entorno Servidor/Tienda/GesTienda/Default.aspx.designer.cs
+++ Entorno Servidor/Tienda/GesTienda/Default.aspx.designer.cs
@@ -31,5 +31,23 @@ namespace GesTienda
         /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Login Login1;
+
+        /// <summary>
+        /// Control LoginButton.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button LoginButton;
+
+        /// <summary>
+        /// Control lblMensajes.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblMensajes;
     }
 }
